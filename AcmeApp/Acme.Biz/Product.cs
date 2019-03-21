64263d1682890c74d0a64f9c9428dcbef4684fd7
Benjamin West <benjaminw6633@hotmail.com>
2019-03-21 15:20:36 +0000

    Properties

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -22,6 +22,7 @@ namespace Acme.Biz
             Console.WriteLine("Product instance created");
             //this.ProductVendor = new Vendor();
             this.MinimumPrice = .96m;
+            this.Category = "Tools";
         }
 
         public Product(int productId,
@@ -55,8 +56,26 @@ namespace Acme.Biz
 
         public string ProductName
         {
-            get { return productName; }
-            set { productName = value; }
+            get
+            {
+                var formattedValue = productName?.Trim();
+                return formattedValue;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    ValidationMessage = "Product Name must be at least 3 characters";
+                }
+                else if (value.Length > 20)
+                {
+                    ValidationMessage = "Product Name cannot be more than 20 characters";
+                }
+                else
+                {
+                    productName = value;
+                }
+            }
         }
         private string description;
 
@@ -86,6 +105,12 @@ namespace Acme.Biz
             }
             set { productVendor = value; }
         }
+        internal string Category { get; set; }
+        public int SequenceNumber { get; set; } = 1;
+
+        public string ProductCode => this.Category + "-" + this.SequenceNumber;
+
+        public string ValidationMessage { get; private set; }
 
         #endregion
 
